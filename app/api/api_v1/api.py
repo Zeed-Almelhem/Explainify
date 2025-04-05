a4a711d5b653b989f6ee8a1d0ca80e2f997bd101
Zeed Almelhem <zeed.almelhem@zeed-almelhem.com>
2025-04-05 10:01:54 -0700

    Delete all the fies created by AI

--- app/api/api_v1/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.api_v1.endpoints import models, explanations
-
-api_router = APIRouter()
-
-api_router.include_router(models.router, prefix="/models", tags=["models"])
-api_router.include_router(explanations.router, prefix="/explanations", tags=["explanations"])
